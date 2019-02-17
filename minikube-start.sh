9034e14d13ff31309e58bc9a332566d7a287065c
Lukasz Bielinski <l.bielinski@f5.com>
2019-02-17 13:51:10 +0100

    update

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-minikube delete && minikube start --kubernetes-version=v1.13.2 --memory=4096 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
+#minikube delete &&
+minikube start --kubernetes-version=v1.13.2 --cpus 4 --memory=6096 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
 
 #minikube addons enable ingress
